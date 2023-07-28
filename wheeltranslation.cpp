f687212c7dec7be75db51577af15a3329822151c
romistaro <77026665+romistaro@users.noreply.github.com>
2023-07-28 19:03:06 -0400

    Update wheeltranslation.cpp

--- wheeltranslation.cpp
+++ wheeltranslation.cpp
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include <cmath>
 
+int wheelpower(double joy_x, double joy_y, wheel_offset){ 
+
+  return(sin(wheel_offset -( atan(180/pi * (joy_y/joy_x)))) * sqrt(pow(joy_x,2) + pow(joy_y,2)))
+
+  
+}
